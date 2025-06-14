400ca53a27b5cda8c9dfa1c5ee8f913c5cf8b65a
Mounir Nessim <mmnessim@gmail.com>
2025-06-14 06:32:12 -0400

    take zipcode from query in SendWeather()

--- main.go
+++ main.go
@@ -16,7 +16,9 @@ const (
 func SendWeather(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
 	fmt.Printf("Recieved request at %s\n", now.String())
-	l := weather.GetLatAndLong("27713")
+	//fmt.Printf("URL: %s\n", r.URL)
+	zipcode := r.URL.Query().Get("zipcode")
+	l := weather.GetLatAndLong(zipcode)
 	weath := weather.GetWeatherWithLatAndLong(l.Lat, l.Long)
 	fmt.Println(weath)
 	fmt.Fprintf(w, "cur=%s high=%s low=%s weather=%s precip=%s",
@@ -35,7 +37,7 @@ func SendWeatherWithZip(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	_ = weather.GetLatAndLong("27713")
+	//_ = weather.GetLatAndLong("27713")
 
 	weather.GetWeather()
 	http.HandleFunc("/", SendWeather)
